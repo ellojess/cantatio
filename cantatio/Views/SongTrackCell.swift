b74a18295a523fd5f076f1eceda61d910fb3ed38
Jess <jtrinh619@gmail.com>
2020-10-01 12:36:20 -0700

    animate buttons

--- cantatio/Views/SongTrackCell.swift
+++ cantatio/Views/SongTrackCell.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class SongTrackCell: UITableViewCell {
     
+    var linkVC: FavSongsVC?
+    
     var albumImage: UIImageView = {
         var image = UIImageView()
         image.layer.cornerRadius = 10
@@ -50,12 +52,15 @@ class SongTrackCell: UITableViewCell {
         button.setImage(UIImage(named: "icon_favorite"), for: .normal)
         button.imageView?.contentMode = .scaleAspectFit
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(favTapped), for: .touchDown)
         return button
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupCell()
+        favoriteButton.addTarget(self, action: #selector(favTapped), for: .touchDown)
+        playButton.addTarget(self, action: #selector(playPauseTapped), for: .touchDown)
     }
     
     required init?(coder: NSCoder) {
@@ -86,6 +91,43 @@ class SongTrackCell: UITableViewCell {
    
     }
     
+    @objc func favTapped(){
+        if self.favoriteButton.currentImage == UIImage(named: "icon_favorite"){
+            
+            UIView.animate(withDuration: 0.3,
+            animations: {
+                self.favoriteButton.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            },
+            completion: { _ in
+                UIView.animate(withDuration: 0.3) {
+                    self.favoriteButton.transform = CGAffineTransform.identity
+                }
+            })
+            
+            self.favoriteButton.setImage(UIImage(named: "icon_favorite-filled"), for: .normal)
+        } else {
+            self.favoriteButton.setImage(UIImage(named: "icon_favorite"), for: .normal)
+        }
+    }
+    
+    @objc func playPauseTapped(){
+        if self.playButton.currentImage == UIImage(named: "icon_play"){
+            
+            UIView.animate(withDuration: 0.3,
+            animations: {
+                self.playButton.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            },
+            completion: { _ in
+                UIView.animate(withDuration: 0.3) {
+                    self.playButton.transform = CGAffineTransform.identity
+                }
+            })
+            
+            self.playButton.setImage(UIImage(named: "icon_pause"), for: .normal)
+        } else {
+            self.playButton.setImage(UIImage(named: "icon_play"), for: .normal)
+        }
+    }
     
     
 }

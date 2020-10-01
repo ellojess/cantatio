330205419dc6b41d1d869eff01d40334c254d10b
Jess <jtrinh619@gmail.com>
2020-10-01 01:09:24 -0700

    begin implementing network for tracks

---
+++ cantatio/Models/Track.swift
@@ -0,0 +1,15 @@
+//
+//  Track.swift
+//  cantatio
+//
+//  Created by Jessica Trinh on 10/1/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import Foundation
+
+struct Track {
+    let mainImage : UIImage!
+    let name : String!
+    let previewURL : String!
+}
---
+++ cantatio/Network/NetworkManager.swift
@@ -0,0 +1,82 @@
+//
+//  NetworkManager.swift
+//  cantatio
+//
+//  Created by Jessica Trinh on 10/1/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Spartan
+
+class NetworkManager {
+    
+    // MARK: - Variables
+    public static let shared = NetworkManager()
+    let urlSession = URLSession.shared
+    
+    typealias JSONStandard = [String : AnyObject]
+    var posts = [Track]()
+    
+    public static var authorizationToken: String?
+    public static var loggingEnabled: Bool = true
+    
+    
+    var artistId = ""
+    
+    var refresh_token: String? = nil
+    
+    
+    // Fetch Top 50 Artists
+    static func fetchTopArtists(completion: @escaping (Result<[Artist], Error>) -> Void) {
+        
+        _ = Spartan.getMyTopArtists(limit: 50, offset: 0, timeRange: .mediumTerm, success: { (pagingObject) in
+            completion(.success(pagingObject.items))
+
+        }, failure: { (error) in
+            completion(.failure(error))
+        })
+    }
+    
+    // Fetch Artist's Top Tracks
+//    static func fetchTopTracks(completion: @escaping (Result<[Artist], Error>) -> Void) {
+//        _ = Spartan.getArtistsTopTracks(artistId: artistId, country: .us, success: { (tracks) in
+//           // Do something with the tracks
+//       }, failure: { (error) in
+//           print(error)
+//       })
+//    }
+    
+    func parseData(JSONData : Data) {
+        do {
+            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
+            if let tracks = readableJSON["tracks"] as? JSONStandard{
+                if let items = tracks["items"] as? [JSONStandard] {
+                    for i in 0..<items.count{
+                        let item = items[i]
+                        print(item)
+                        let name = item["name"] as! String
+                        let previewURL = item["preview_url"] as! String
+                        if let album = item["album"] as? JSONStandard{
+                            if let images = album["images"] as? [JSONStandard]{
+                                let imageData = images[0]
+                                let mainImageURL =  URL(string: imageData["url"] as! String)
+                                let mainImageData = NSData(contentsOf: mainImageURL!)
+                                
+                                let mainImage = UIImage(data: mainImageData! as Data)
+                                
+                                posts.append(Track.init(mainImage: mainImage, name: name, previewURL: previewURL))
+//                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+}
